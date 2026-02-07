1b5cc0f5f9fdff31d5712860ab9387f46c8319a5
Nkeiru Lois <nkeiruloisy@yahoo.com>
2026-02-07 03:39:49 +0100

    Add Tailwind React project properly as a folder

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-4 sm:py-8">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
